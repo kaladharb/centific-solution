191acb45d79cfc4ee3a18fe3bf92c0f5b7e07300
kaladharb <kaladharbandari@gmail.com>
2026-04-05 09:49:57 +0530

    Fix backend deployment

--- backend/app.py
+++ backend/app.py
@@ -20,6 +20,10 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
     app.config.from_object(Config)
 
     db.init_app(app)
